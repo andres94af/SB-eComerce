7d0e2148fd9f7c046441e53c6f60b7e1ecef33b4
Andres Fernandez <andres94.af@gmail.con>
2022-12-12 09:46:27 +0100

    funcionalidad ver productos para el usuario(metodo home, directorio y template home)

--- src/main/java/com/curso/ecomerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecomerce/controller/ProductoController.java
@@ -74,14 +74,13 @@ public class ProductoController {
 	
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p=productoService.get(producto.getId()).get();
+		
 		if (file.isEmpty()) {//cuando se edita el prod y no se cambia la img
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
+
 			producto.setImagen(p.getImagen());
 		}else {//cuando se edita tbn la imagen
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
-			
 			//eliminar cuando no sea la img por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
@@ -90,7 +89,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
-		
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
