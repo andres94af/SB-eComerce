c7fe0f544a9c3b51044af1d8e39e976b41443788
Andres Fernandez <andres94.af@gmail.con>
2022-12-13 11:49:20 +0100

    mostrar informacion de usuario y detalle de carrito en la vista resumen orden

---
+++ src/main/java/com/curso/ecomerce/service/UsuarioServiceImpl.java
@@ -0,0 +1,22 @@
+package com.curso.ecomerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.ecomerce.model.Usuario;
+import com.curso.ecomerce.repository.IUsuarioRepository;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+	
+	@Autowired
+	private IUsuarioRepository usuarioRepository;
+
+	@Override
+	public Optional<Usuario> findById(Integer id) {
+		return usuarioRepository.findById(id);
+	}
+
+}
