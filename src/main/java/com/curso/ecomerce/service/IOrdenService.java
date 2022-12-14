e6d3ddef784eeab06bda52a74ea979ce5aa63d77
Andres Fernandez <andres94.af@gmail.con>
2022-12-14 10:55:57 +0100

    metodo para generar numero de orden

--- src/main/java/com/curso/ecomerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecomerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.curso.ecomerce.service;
 
+import java.util.List;
+
 import com.curso.ecomerce.model.Orden;
 
 public interface IOrdenService {
-	Orden save(Orden orden);
+	public List<Orden> findAll();
+	public Orden save(Orden orden);
 }
--- src/main/java/com/curso/ecomerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecomerce/service/OrdenServiceImpl.java
@@ -1,5 +1,7 @@
 package com.curso.ecomerce.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,16 @@ public class OrdenServiceImpl implements IOrdenService{
 	@Override
 	public Orden save(Orden orden) {
 		return ordenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		return ordenRepository.findAll();
+	}
+	
+	public String generarNumeroOrden() {
+		
+		return "";
 	}
 
 }
